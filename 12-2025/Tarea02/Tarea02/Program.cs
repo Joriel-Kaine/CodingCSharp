ad005f3b0faefd129a76810e3d79317fb51d793f
Manuel MR <manuel.monred@gmail.com>
2025-12-02 02:22:58 +0100

    Training

---
+++ 12-2025/Tarea02/Tarea02/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Tarea02
+{
+    internal class Program
+    {
+        static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static bool ContinuarPeticion(string respuesta)
+        {
+            bool esCorrecto = false;
+
+            if (respuesta == "S" || respuesta == "s")
+            {
+                esCorrecto = true;
+            }
+            else if (respuesta == "N" || respuesta == "n")
+            {
+                esCorrecto = false;
+            }
+
+            return esCorrecto;
+        }
+
+        static int DevolverAbsoluto(int num)
+        {
+            int absoluto;
+            return absoluto = num > 0 ? num : -num;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nC# - Tarea 02: Manuel MR ®\n" +
+                              "══════════════════════════\n\n");
+
+            int num;
+            string respuesta = "";
+
+            do
+            {
+                num = LeerNumero("\nIntroduce un número: ");
+                Console.WriteLine($"\n═> El valor absoluto de {num} es {DevolverAbsoluto(num)}");
+
+                do
+                {
+                    Console.Write("\n¿Desea introducir otro número? S/N: ");
+                    respuesta = Console.ReadLine();
+
+                    if ((respuesta != "S" && respuesta != "s") &&
+                          (respuesta != "N" && respuesta != "n"))
+                    {
+                        Console.WriteLine("\nDebes introducir \"S\" o \"N\".");
+                    }
+
+                } while ((respuesta != "S" && respuesta != "s") &&
+                          (respuesta != "N" && respuesta != "n"));
+
+            } while (ContinuarPeticion(respuesta));
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
